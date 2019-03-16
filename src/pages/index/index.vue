ce95bf8db62a13d38a144addafc52445df7c45ec
hpy617559 <1103876865@qq.com>
2019-03-16 16:43:37 +0800

    轮播图

---
+++ src/pages/index/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <swiper
+      :indicator-dots="indicatorDots"
+      indicatorColor="rgba(255,255,255,0.3)"
+      indicatorActiveColor="rgba(255,255,255,1)"
+      :autoplay="autoplay"
+      :interval="interval"
+      :duration="duration"
+      :circular="circular"
+    >
+      <block v-for="(item,index) in imgUrls" :key="index">
+        <swiper-item>
+          <image :src="item" class="slide-image" mode="aspectFill"></image>
+        </swiper-item>
+      </block>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imgUrls: [
+        "https://lg-igjc8p1o-1256763078.cos.ap-shanghai.myqcloud.com/upload/banner1.png",
+        "https://lg-igjc8p1o-1256763078.cos.ap-shanghai.myqcloud.com/upload/banner2.png",
+        "https://lg-igjc8p1o-1256763078.cos.ap-shanghai.myqcloud.com/upload/banner3.png"
+      ],
+      circular:true,
+      // indicatorColor:"rgba(255,255,255,0.3)",
+      // indicatorActiveColor:"rgba(255,255,255,1)",
+      indicatorDots: true,
+      autoplay: true,
+      interval: 5000,
+      duration: 1000,
+      
+    };
+  },
+
+  components: {},
+
+  methods: {},
+
+  created() {
+    // let app = getApp()
+  }
+};
+</script>
+
+<style>
+swiper{
+  height:340rpx;
+}
+image {
+  width:100%;
+  height:100%;
+}
+</style>
