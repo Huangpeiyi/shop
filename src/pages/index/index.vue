3bcc5a976e98f79c1c0d4119a724e6b1aa6f3441
hpy617559 <1103876865@qq.com>
2019-03-16 20:33:17 +0800

    首页完成

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+
+    <!-- 搜索 -->
+    <view class="search-in">
+      <navigator url="/pages/search/main" hover-class="none"><icon type="search" size="16" /> 搜索</navigator>
+    </view>
+    <!-- 搜索 end-->
+
+    <!-- 轮播图 -->
     <swiper
       :indicator-dots="indicatorDots"
       indicatorColor="rgba(255,255,255,0.3)"
@@ -15,6 +23,33 @@
         </swiper-item>
       </block>
     </swiper>
+    <!-- 轮播图 end-->
+
+    <!-- 导航按钮 -->
+    <view class="navBox">
+      <image src="https://lg-igjc8p1o-1256763078.cos.ap-shanghai.myqcloud.com/upload/icon_index_nav_4@2x.png" mode="aspectfill"></image>
+      <image src="https://lg-igjc8p1o-1256763078.cos.ap-shanghai.myqcloud.com/upload/icon_index_nav_3@2x.png" mode="aspectfill"></image>
+      <image src="https://lg-igjc8p1o-1256763078.cos.ap-shanghai.myqcloud.com/upload/icon_index_nav_2@2x.png" mode="aspectfill"></image>
+      <image src="https://lg-igjc8p1o-1256763078.cos.ap-shanghai.myqcloud.com/upload/icon_index_nav_1@2x.png" mode="aspectfill"></image>
+    </view>
+    <!-- 导航按钮 end-->
+
+    <!-- 产品类别 -->
+    <view class="pro">
+      <view class="h1">产品类别</view>
+      <view class="proBox">
+        <view class="proLeft">
+          <image src="https://lg-igjc8p1o-1256763078.cos.ap-shanghai.myqcloud.com/upload/pic_floor01_1@2x.png"></image>
+        </view>
+        <view class="proRight">
+          <image src="https://lg-igjc8p1o-1256763078.cos.ap-shanghai.myqcloud.com/upload/pic_floor01_2@2x.png"></image>
+          <image src="https://lg-igjc8p1o-1256763078.cos.ap-shanghai.myqcloud.com/upload/pic_floor01_3@2x.png"></image>
+          <image src="https://lg-igjc8p1o-1256763078.cos.ap-shanghai.myqcloud.com/upload/pic_floor01_4@2x.png"></image>
+          <image src="https://lg-igjc8p1o-1256763078.cos.ap-shanghai.myqcloud.com/upload/pic_floor01_5@2x.png"></image>
+        </view>
+      </view>
+    </view>
+    <!-- 产品类别 end-->
   </div>
 </template>
 
@@ -55,5 +90,64 @@ swiper{
 image {
   width:100%;
   height:100%;
+}
+.search-in{
+  width:750rpx;
+  height:100rpx;
+  background-color: #eb4450;
+  padding:20rpx;
+  box-sizing:border-box;
+}
+.search-in navigator {
+  width:100%;
+  height:60rpx;
+  background:#fff;
+  border-radius: 4px;
+  display:flex;
+  justify-content: center;
+  align-items: center;
+  font-size:24rpx;
+}
+.search-in navigator icon{
+  margin-right:10rpx;
+}
+.proBox{
+  width:750rpx;
+  height:386rpx;
+  display:flex;
+  padding:0 10rpx;
+  box-sizing:border-box;
+}
+.proLeft{
+  flex:1;
+}
+.proRight{
+  flex:2;
+}
+.proRight image{
+  width:48%;
+  height:190rpx;
+  margin-left:10rpx;
+}
+.proBox h1{
+  width:750rpx;
+}
+.pro .h1{
+  width:750rpx;
+  height:60rpx;
+  line-height:60rpx;
+  padding:10rpx;
+  font-size:16px;
+}
+.navBox{
+  width:750rpx;
+  height:140rpx;
+  display:flex;
+  justify-content: space-around;
+  padding:20rpx 0;
+}
+.navBox image{
+  width:128rpx;
+  height:140rpx;
 }
 </style>
---
+++ src/pages/search/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    搜索页面
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+     
+    }
+  },
+
+  components: {
+    
+  },
+
+  methods: {
+    
+  },
+
+  created () {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
